568128e7b41c89941da5e40fe3dd82040286461e
preciousrodenyi-ctrl <preciousrodenyi@gmail.com>
2026-05-13 05:58:14 +0300

    Added property cards and navbar

---
+++ src/components/Navbars.jsx
@@ -0,0 +1,22 @@
+<nav className="bg-white shadow-md px-8 py-4 flex justify-between items-center">
+
+  {/* Logo */}
+  <h1 className="text-2xl font-bold text-slate-900">
+ RealEstate Hub
+  </h1>
+    {/* Navigation Links */}
+  <div className="flex gap-6 text-gray-700 font-medium">
+    <a href="/">Home</a>
+    <a href="/properties">Properties</a>
+    <a href="/favorites">Favorites</a>
+    <a href="/add">Add Property</a>
+  </div>
+
+  {/* Icons */}
+  <div className="flex gap-4 items-center">
+    <button>search</button>
+
+    <div className="w-10 h-10 bg-slate-900 rounded-full"></div>
+  </div>
+
+</nav>
