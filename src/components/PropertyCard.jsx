e95af7ad0771fe84786ae2cde59b03ce82c070f4
preciousrodenyi-ctrl <preciousrodenyi@gmail.com>
2026-05-13 12:52:00 +0300

    propertycard change

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -14,14 +14,16 @@
 
     <select className="border p-3 rounded-lg">
       <option>Apartment</option>
-      <option>Villa</option>
-      <option>Bungalow</option>
+      <option>House</option>
+      <option>Land</option>
     </select>
 
     <select className="border p-3 rounded-lg">
-      <option>Any Price</option>
-      <option>Under 50k</option>
-      <option>50k - 100k</option>
+      <option>1500</option>
+      <option>1200</option>
+      <option>2500</option>
+      <option>50000</option>
+      <option>3500</option>
     </select>
 
     <button className="bg-slate-900 text-white rounded-lg">
