f4612fe7c11d9f2855d25295975194dd67076055
preciousrodenyi-ctrl <preciousrodenyi@gmail.com>
2026-05-15 19:24:46 +0300

    finishing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,5 @@
 import Hero from "../components/Hero";
 
-<<<<<<< HEAD
-export default function Home() {
-  return (
-    <div>
-      <Hero />
-      <section className="py-12">
-
-        <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-6 text-center">
-            Welcome to Your Dream Home Finder
-          </h2>
-          <p className="text-lg text-gray-700 mb-8 text-center">
-            Discover the best properties in your desired location. Whether you're looking for a cozy apartment, a spacious house, or vacant land, we have it all.
-          </p>
-        </div>
-      </section>
-    </div>
-  );
-}
-=======
 function Home() {
   return (
     <div>
@@ -31,4 +11,3 @@ function Home() {
 }
 
 export default Home;
->>>>>>> master
